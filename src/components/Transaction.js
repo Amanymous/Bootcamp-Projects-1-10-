9271b41ab22e5e70b84687cab2121f26f054c0ad
amanymous <aman.mirza9@yahoo.com>
2020-11-18 23:49:10 +0500

    Expense tracker completed

---
+++ src/components/Transaction.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "./contextApi/GlobalState";
+
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        delete
+      </button>
+    </li>
+  );
+};
